cd92be94fd3ccd2198526eca5fa98a2bd72534cb
Cek <silviacecuttimsa@gmail.com>
2020-06-22 00:23:43 +0200

    display one project page

--- template/page_projet_seul.php
+++ template/page_projet_seul.php
@@ -7,29 +7,35 @@
   $projetAAfficher = $bdd -> query ("select nom_projet, description, url_image, annee, client, lien FROM projet WHERE id_projet = ($_GET[projetAAfficher])") -> fetch();
 
   $technoAAfficher = $bdd -> query ("select nom_techno from techno, projet_techno, projet where techno.id_techno = projet_techno.techno_id and projet.id_projet = ($_GET[projetAAfficher]) and projet_techno.projet_id = projet.id_projet") -> fetchAll();
-  // var_dump($technoAAfficher);
+?>
+<div class="un_projet">
 
-  echo "<h1>$projetAAfficher[nom_projet]</h1>";
+  <?php
 
-
-
-foreach ($projetAAfficher as $id_projet) {
-
-  echo html_image("$projetAAfficher[url_image].jpg", "img_pt");
-//   // echo html_image("template/img/$id_projet._2.jpg", "img_pt");
-//   // echo html_image("template/img/$id_projet._3.jpg", "img_pt");
-}
-
-  foreach ($technoAAfficher as $key => $techno) {
-    echo "<div>$techno[nom_techno]<div>";
-  }
-
-  echo "<p>$projetAAfficher[description]</p>";
-
-  echo "<p>$projetAAfficher[annee], $projetAAfficher[client]</p>";
-
-  echo "<div>$projetAAfficher[lien]";
+    echo "<h1>$projetAAfficher[nom_projet]</h1>";
 
 
+    echo html_image("$projetAAfficher[url_image].jpg", "img_pt");
 
-  include "include/footer.php";
+?>
+    <div class="one">
+
+    <?php
+    foreach ($technoAAfficher as $key => $techno) {
+      echo "$techno[nom_techno]<br>";
+    }
+    ?>
+    </div>
+
+    <?php
+
+    echo "<div class='two'><p>$projetAAfficher[description]</p></div>";
+
+    echo "<div class='three'><p>Réalisé en $projetAAfficher[annee] pour $projetAAfficher[client]</p></div>";
+
+    echo "<div class='four'><p>$projetAAfficher[lien]</p></div>";
+
+    ?>
+    </div>
+<!-- créer une pagination avec <a>page suivante ></a> qui incrémente la valeur du paramètre de GET de la page (id_projet) d'un. Si le num n'est pas un id_projet, de deux...-->
+  <?php include "include/footer.php";
